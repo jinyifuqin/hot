fee71e8d1dbc4f023dae8989ff9d05e1bb02b75b
jinyifuqin <jinyifuqin@163.com>
2018-04-05 11:50:05 +0800

    saveUser

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -23,9 +23,48 @@ use Cache;
 
 class UsersController extends Controller
 {
-  public function index()
-    {
+    public function index(){
         return view('admin.index');
         //
     }
+
+    public function register(){
+        return view('admin.register');
+    }
+
+    public function save(Request $req)
+    {   //
+//        $id = DB::table('users')->insertGetId(
+//            ['email' => 'john@example.com', 'votes' => 0]);
+        $obj = new Newuser();
+        $obj->username = $req['username'];
+        $obj->email = $req['email'];
+        $obj->password = $req['psw'];
+        $str = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        $newStr = '';
+        for($i=0;$i<14;$i++){
+            $num = mt_rand(0,strlen($str)-1);
+            $newStr .= substr($str,$num,1);
+        }
+        $newStr = $newStr.'.jpg';
+        if($req->file('picname'))
+            $req->file('picname')->move(public_path('uploads'),$newStr);
+        $obj->headpic = $newStr;
+
+
+        $re = $obj->save();
+
+        if($re){
+            return view('/admin/wait')->with(
+                [
+                    'message'=>'你已经注册成功，请等待！',
+                    'url' =>'/admin/user',
+                    'jumpTime'=>8,
+                ]
+            );
+        }else{
+            return false;
+        }
+    }
+
 }
---
+++ resources/views/admin/register.blade.php
@@ -0,0 +1,45 @@
+@extends('layout')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3 style="text-align: center">用户注册</h3>
+            </div>
+        </div>
+        <form enctype="multipart/form-data" class="form-horizontal" role="form" action="/admin/save" method="post">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <div class="col-sm-2"></div>
+                <label for="firstname" class="col-sm-2 control-label">用户名：</label>
+                <div class="col-sm-5">
+                    <input type="text" name="username" class="form-control" id="firstname" placeholder="请输入名字">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-2"></div>
+                <label for="secname" class="col-sm-2 control-label">电子邮件：</label>
+                <div class="col-sm-5">
+                    <input type="email" name="email" class="form-control" id="secname" placeholder="请输入名字">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-2"></div>
+                <label for="thname" class="col-sm-2 control-label">密码：</label>
+                <div class="col-sm-5">
+                    <input type="password" name="psw" class="form-control" id="thname" placeholder="请输入名字">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputfile" class="col-sm-2 col-sm-offset-2 control-label">头像上传：</label>
+                <input class="col-sm-offset-4" name="picname" type="file" id="inputfile">
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-4 col-sm-2">
+                    <button type="submit" class="btn-lg btn btn-primary">注册</button>
+                </div>
+            </div>
+        </form>
+
+    </div>
+
+@stop
