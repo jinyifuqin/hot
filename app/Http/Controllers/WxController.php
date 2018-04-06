13465bbad8d334d25b82563c99c0635cb830f224
yangyh <jinyifuqin@163.com>
2018-04-06 20:10:10 +0800

    weibo

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -1,91 +1,65 @@
 <?php
-//namespace App\Http\Controllers;
-use Illuminate\Routing\Controller;
-
-
-header('content-type:text/html;charset=utf-8');
-
-define("TOKEN", "hello"); //define your token
-
-$wx = new wechatCallbackapiTest();
-
-if($_GET['echostr']){
-    $wx->valid(); //如果发来了echostr则进行验证
+define("TOKEN", "hello");
+$wechatObj = new wechatCallbackapiTest();
+if (isset($_GET['echostr'])) {
+    $wechatObj->valid();
 }else{
-    $wx->responseMsg(); //如果没有echostr，则返回消息
+    $wechatObj->responseMsg();
 }
-
-
-class wechatCallbackapiTest{
-
-    public function valid(){ //valid signature , option
-
+class wechatCallbackapiTest
+{
+    public function valid()
+    {
         $echoStr = $_GET["echostr"];
-        if($this->checkSignature()){ //调用验证字段
+        if($this->checkSignature()){
+            header('content-type:text');
             echo $echoStr;
             exit;
         }
     }
-
-    public function responseMsg(){
-
-        //get post data, May be due to the different environments
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"]; //接收微信发来的XML数据
-
-        //extract post data
-        if(!empty($postStr)){
-
-            //解析post来的XML为一个对象$postObj
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-
-            $fromUsername = $postObj->FromUserName; //请求消息的用户
-            $toUsername = $postObj->ToUserName; //"我"的公众号id
-            $keyword = trim($postObj->Content); //消息内容
-            $time = time(); //时间戳
-            $msgtype = 'text'; //消息类型：文本
-            $textTpl = "<xml>  
-                        <ToUserName><![CDATA[%s]]></ToUserName>  
-                        <FromUserName><![CDATA[%s]]></FromUserName>  
-                        <CreateTime>%s</CreateTime>  
-                        <MsgType><![CDATA[%s]]></MsgType>  
-                        <Content><![CDATA[%s]]></Content>  
-                        </xml>";
-
-            if($keyword == 'hehe'){
-                $contentStr = 'hello world!!!';
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgtype, $contentStr);
-                echo $resultStr;
-                exit();
-            }else{
-                $contentStr = '输入hehe试试';
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgtype, $contentStr);
-                echo $resultStr;
-                exit();
-            }
-
-        }else {
-            echo "dddd";
-            exit;
-        }
-    }
-
-    //验证字段
-    private function checkSignature(){
-
+    private function checkSignature()
+    {
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
-
         $token = TOKEN;
         $tmpArr = array($token, $timestamp, $nonce);
-        sort($tmpArr);
+        sort($tmpArr, SORT_STRING);
         $tmpStr = implode( $tmpArr );
         $tmpStr = sha1( $tmpStr );
-
         if( $tmpStr == $signature ){
             return true;
         }else{
             return false;
+        }
+    }
+    public function responseMsg()
+    {
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        if (!empty($postStr)){
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA); //获取数据
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+            <ToUserName><![CDATA[%s]]></ToUserName>
+            <FromUserName><![CDATA[%s]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[%s]]></MsgType>
+            <Content><![CDATA[%s]]></Content>
+            <FuncFlag>0</FuncFlag>
+            </xml>";
+            if($keyword == "?" || $keyword == "？") //获取用户信息
+            {
+                $msgType = "text";
+                $contentStr = date("Y-m-d H:i:s",time()); // 回复的内容
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }
+        }else{
+            echo "";
+            exit;
         }
     }
 }
