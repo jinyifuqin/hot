e427a1a440d120b7cc3a3bb664f226898bc7bb2f
yangyh <jinyifuqin@163.com>
2018-04-06 19:04:12 +0800

    weibo

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -8,30 +8,33 @@
 
 namespace App\Http\Controllers;
 define("TOKEN", "yangyh");//自己定义的token 就是个通信的私钥
-//$wechatObj = new \WeixinController();
-//$wechatObj->valid();
-//$wechatObj->responseMsg();
+
 class WeixinController extends Controller
 {
     public function __construct()
     {
-        $this->responseMsg();
+        if (isset($_GET['echostr'])) {
+            $this->valid();
+        }else{
+            $this->responseMsg();
+        }
     }
 
     public function valid()
     {
         $echoStr = $_GET["echostr"];
-//        var_dump($echoStr);exit;
         if($this->checkSignature()){
+            header('content-type:text');
             echo $echoStr;
             exit;
         }
     }
+
     public function responseMsg()
     {
         $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-        if (!empty($postStr)){ //s
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+        if (!empty($postStr)){
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA); //获取数据
             $fromUsername = $postObj->FromUserName;
             $toUsername = $postObj->ToUserName;
             $keyword = trim($postObj->Content);
@@ -42,31 +45,60 @@ class WeixinController extends Controller
             <CreateTime>%s</CreateTime>
             <MsgType><![CDATA[%s]]></MsgType>
             <Content><![CDATA[%s]]></Content>
-            <FuncFlag>0<FuncFlag>
+            <FuncFlag>0</FuncFlag>
             </xml>";
-            if(!empty( $keyword ))
+            if($keyword == "?" || $keyword == "？") //获取用户信息
             {
                 $msgType = "text";
-                $contentStr = '你好啊，屌丝';
+                $contentStr = date("Y-m-d H:i:s",time()); // 回复的内容
                 $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
                 echo $resultStr;
-            }else{
-                echo '咋不说哈呢';
             }
-        }else {
-            echo '咋不说哈呢';
+        }else{
+            echo "";
             exit;
         }
     }
+//    public function responseMsg()
+//    {
+//        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+//        if (!empty($postStr)){ //s
+//            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+//            $fromUsername = $postObj->FromUserName;
+//            $toUsername = $postObj->ToUserName;
+//            $keyword = trim($postObj->Content);
+//            $time = time();
+//            $textTpl = "<xml>
+//            <ToUserName><![CDATA[%s]]></ToUserName>
+//            <FromUserName><![CDATA[%s]]></FromUserName>
+//            <CreateTime>%s</CreateTime>
+//            <MsgType><![CDATA[%s]]></MsgType>
+//            <Content><![CDATA[%s]]></Content>
+//            <FuncFlag>0<FuncFlag>
+//            </xml>";
+//            if(!empty( $keyword ))
+//            {
+//                $msgType = "text";
+//                $contentStr = '你好啊，屌丝';
+//                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+//                echo $resultStr;
+//            }else{
+//                echo '咋不说哈呢';
+//            }
+//        }else {
+//            echo '咋不说哈呢';
+//            exit;
+//        }
+//    }
 
     private function checkSignature()
     {
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
-        $token =TOKEN;
+        $token = TOKEN;
         $tmpArr = array($token, $timestamp, $nonce);
-        sort($tmpArr);
+        sort($tmpArr, SORT_STRING);
         $tmpStr = implode( $tmpArr );
         $tmpStr = sha1( $tmpStr );
         if( $tmpStr == $signature ){
