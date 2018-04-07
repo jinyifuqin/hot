f83b9732b5c6f82913e4d6a69142e9961035ccc7
yangyh <jinyifuqin@163.com>
2018-04-07 11:24:14 +0800

    weibo

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -4,25 +4,30 @@ use App\Http\Controllers\Controller; //addaa
 use Log;
 class WeixinController extends Controller {
     public function index(){
-        Log::info('yangyhlog',$_REQUEST);
-        $timestamp = $_GET['timestamp'];//timestamp其实就是一个时间戳
-        $nonce     = $_GET['nonce'];//nonce是一个随机参数
-        $token     = "weixin";//这个token填写你在微信公众平台上写的那个值
-        $signature = $_GET['signature'];//这个signature其实就是在微信公众平台已经加密好的字符串
-        $echostr   = $_GET['echostr'];
-        $array     = array( $timestamp, $nonce, $token);
-        sort($array);
+        if(empty($_GET)){
+            Log::info('newyyh',$_REQUEST);
+            $timestamp = $_GET['timestamp'];//timestamp其实就是一个时间戳
+            $nonce     = $_GET['nonce'];//nonce是一个随机参数
+            $token     = "weixin";//这个token填写你在微信公众平台上写的那个值
+            $signature = $_GET['signature'];//这个signature其实就是在微信公众平台已经加密好的字符串
+            $echostr   = $_GET['echostr'];
+            $array     = array( $timestamp, $nonce, $token);
+            sort($array);
 
-        $tmpstr = implode('', $array);
-        $tmpstr = sha1($tmpstr);
+            $tmpstr = implode('', $array);
+            $tmpstr = sha1($tmpstr);
 
-        if( $tmpstr == $signature && $echostr){
+            if( $tmpstr == $signature && $echostr){
 
-            echo $echostr;
-            exit;
+                echo $echostr;
+                exit;
+            }else{
+                $this->reponseMsg();
+            }
         }else{
             $this->reponseMsg();
         }
+
     }
     public function reponseMsg(){
         $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
