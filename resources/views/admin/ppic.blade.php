e3692d207322df17acaf868b230ad397f3cba87b
yangyh <jinyifuqin@163.com>
2018-04-10 14:18:02 +0800

    模板

---
+++ resources/views/admin/ppic.blade.php
@@ -0,0 +1,48 @@
+@extends('layout')
+@section('content')
+
+    <div class="container" style="padding-top: 2em;">
+        <div class="row">
+            <div class="col-md-1">
+                <a>Welcome,{{$re->username}}</a>
+                <img width="50" class="img-thumbnail" src="/uploads/{{$re->headpic}}"/>
+            </div>
+            <div class="col-md-1 pull-right">
+                <a href="/admin/signout" class="btn-primary btn">退出登录</a>
+                {{--<img width="50" class="img-thumbnail" src="/uploads/{{$re->headpic}}"/>--}}
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-1 pull-right">
+                <a href="/admin/editshow" class=" btn btn-primary" style="text-align: center">添加日志</a>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>编号</th>
+                <th>标题</th>
+                <th>图片展示</th>
+                <th>操作</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($data as $value)
+                <tr>
+                    <td>{{$value->id}}</td>
+                    <td>{{$value->title}}</td>
+                    <td><img width="30" class="img-thumbnail" src="/uploads/{{$value->pathname}}"></td>
+                    <td>
+                        <a href="/admin/piccontent/{{$value->id}}">详情</a>|
+                        <a href="/admin/picdelete/{{$value->id}}">删除</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        {!! $data->render() !!}
+    </div>
+
+@stop
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,10 +7,20 @@
     <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <script src="{{ URL::asset('js/jquery-3.3.1.min.js') }}"></script>
     <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+    {{--引入内容模板--}}
+    {{--<meta name="viewport" content="width=device-width, initial-scale=1" />--}}
+    <!--[if lte IE 8]><script src="/assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="/assets/css/main.css" />
+    <!--[if lte IE 9]><link rel="stylesheet" href="/assets/css/ie9.css" /><![endif]-->
+    <!--[if lte IE 8]><link rel="stylesheet" href="/assets/css/ie8.css" /><![endif]-->
 </head>
 <body>
 @yield('content')
-<script>
-</script>
+        <!-- Scripts -->
+<script src="/assets/js/jquery.min.js"></script>
+<script src="/assets/js/skel.min.js"></script>
+<script src="/assets/js/util.js"></script>
+<!--[if lte IE 8]><script src="/assets/js/ie/respond.min.js"></script><![endif]-->
+<script src="/assets/js/main.js"></script>
 </body>
 </html>
